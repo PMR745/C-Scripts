ff80701df240f38dcc68ad59644652412071a7f5
PMR745 <96468545+PMR745@users.noreply.github.com>
2023-03-15 23:45:40 +0530

    Add files via upload

---
+++ Motion.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Motion : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject winText;
+    float xInput;
+    float zInput;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+        // Destroy(gameObject);
+            rb.AddForce(Vector3.up * 500); 
+
+        }
+
+        // rb.velocity = Vector3.forward*5;
+
+        if(Input.GetKeyDown(KeyCode.R)) {
+            SceneManager.LoadScene("Level2");
+        }
+
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+
+        rb.AddForce(xInput * speed, 0, zInput * speed);
+    }
+
+    private void OnMouseDown() {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Enemy"){
+            // Destroy(gameObject);
+            Destroy(collision.gameObject);
+            winText.SetActive(true);
+        }
+    }
+}
